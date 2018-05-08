77bc87c0cb2060016208c409a9c55b313ff7656a
Martin Karlgren <martin@karlgren.me>
2018-05-08 11:14:10 +0200

    Very first version of module init macros.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+#![feature(proc_macro)]
+extern crate pike_macros;
+
 extern crate serde;
 
 pub mod pike;
 pub mod bindings;
+
+pub mod module {
+  pub use pike_macros::init_pike_module as init_pike_module;
+  pub use pike_macros::pike_export as pike_export;
+}
