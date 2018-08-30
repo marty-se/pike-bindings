d6093cfdfda90fb04ba09c8a791e490b79423a65
Martin Karlgren <marty@roxen.com>
2018-08-30 17:06:48 +0200

    Remove unused quick-error dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
 #![feature(unboxed_closures, fn_traits)]
-#![feature(proc_macro)]
 extern crate pike_macros;
-
 extern crate serde;
 
-#[macro_use]
-extern crate quick_error;
-
 pub mod pike;
-pub mod bindings;
+pub mod ffi;
 
 pub mod module {
   pub use pike_macros::init_pike_module as init_pike_module;
